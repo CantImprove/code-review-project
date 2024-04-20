4f208d1d3f13db810a3447a3fb7981c822abdd2a
CantImprove <michel6563@mail.ru>
2024-04-20 12:01:48 +0300

    добавил проверку на уже отсортированный массив, чтобы избежать лишних итераций.

--- bubblesort.py
+++ bubblesort.py
@@ -1,10 +1,15 @@
-#функция сортировки
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
+        # Установка флага, который будет указывать, были ли сделаны обмены на этой итерации
+        swapped = False
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        # Если на данной итерации не было сделано ни одного обмена, массив уже отсортирован
+        if not swapped:
+            break
     return arr
 
 # Ввод данных пользователем
